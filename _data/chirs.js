a8badf136ecb9eba94413e47cdcc5064c044039b
Garrett Welson <garrettwelson@gmail.com>
2022-03-11 13:55:59 -0800

    Update to 16 chapters finished

--- _data/chirs.js
+++ _data/chirs.js
@@ -1,6 +1,6 @@
 module.exports = function () {
   const total = 57;
-  const finished = 15
+  const finished = 16
   var percent = (finished * 100) / total;
   const progress = parseFloat(percent).toFixed(0);
   const styleString = `width:${progress}%`;
