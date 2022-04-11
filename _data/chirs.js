49ea9da2dcfd50ed0a450f272742c946c83feb00
Garrett Welson <garrettwelson@gmail.com>
2022-04-11 16:19:44 -0700

    Update to 19

--- _data/chirs.js
+++ _data/chirs.js
@@ -1,6 +1,6 @@
 module.exports = function () {
   const total = 57;
-  const finished = 18;
+  const finished = 19;
   var percent = (finished * 100) / total;
   const progress = parseFloat(percent).toFixed(0);
   const styleString = `width:${progress}%`;
