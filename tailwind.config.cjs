9d165f34d5a23f22f919c16896f9562f963dfd53
Garrett Welson <10912255+garrettwelson@users.noreply.github.com>
2023-03-27 15:58:34 -0700

    Add basic structure

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'sans': ['Avenir', 'Avenir Next LT Pro', 'Montserrat', 'Corbel', 'URW Gothic', 'source-sans-pro', 'sans-serif'	]
+		}
 	},
 	plugins: [],
 }
