d9e13d5dbf71532b2ffc436529959e5d97b365ec
Garrett Welson <10912255+garrettwelson@users.noreply.github.com>
2023-06-08 21:24:37 -0700

    Add sitemap addon and robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx(), image()],
+  integrations: [tailwind(), svelte(), mdx(), image(), sitemap()],
   experimental: {
     assets: true
   }
